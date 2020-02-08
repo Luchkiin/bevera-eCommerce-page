e1a3c306ee7c53412007595ba51aa6d051d99a9b
Lucas Rosendahl <34438601+Luchkiin@users.noreply.github.com>
2020-02-08 11:46:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 /*---BACK TO TOP BUTTON---*/
 
 mybutton = document.getElementById("myBtn");
-
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 /*---MENU---*/
 
